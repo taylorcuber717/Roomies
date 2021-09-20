e6bdf0f1124f6756ef0ad64a05162ca366dd4692
jesuspadres <jesuspadres@gmail.com>
2021-09-20 13:24:10 -0700

    TaskCardView

---
+++ Roomies/Views/TaskViews/TaskCardView.swift
@@ -0,0 +1,20 @@
+//
+//  TaskCardView.swift
+//  TaskCardView
+//
+//  Created by Jessy Padres on 9/20/21.
+//
+
+import SwiftUI
+
+struct TaskCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TaskCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        TaskCardView()
+    }
+}
