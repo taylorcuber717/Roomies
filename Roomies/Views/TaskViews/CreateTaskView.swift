ba383900d8c2d3034048185e38070e47475d6447
jesuspadres <jesuspadres@gmail.com>
2021-09-20 13:26:54 -0700

    Tasks Views

---
+++ Roomies/Views/TaskViews/CreateTaskView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateTaskView.swift
+//  CreateTaskView
+//
+//  Created by Jessy Padres on 9/20/21.
+//
+
+import SwiftUI
+
+struct CreateTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateTaskView()
+    }
+}
---
+++ Roomies/Views/TaskViews/TasksPageView.swift
@@ -0,0 +1,20 @@
+//
+//  TasksPageView.swift
+//  TasksPageView
+//
+//  Created by Jessy Padres on 9/20/21.
+//
+
+import SwiftUI
+
+struct TasksPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TasksPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        TasksPageView()
+    }
+}
